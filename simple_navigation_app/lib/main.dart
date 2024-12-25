1fa404913c6641425df843de34465c04ce7936c6
Hashini Jayasinghe <71764759+HashiniJayasinghe99@users.noreply.github.com>
2024-12-25 06:17:04 +0000

    Initial commit: Flutter button navigation app

---
+++ simple_navigation_app/lib/main.dart
@@ -0,0 +1,73 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'page1.dart';
+import 'page2.dart';
+import 'page3.dart';
+import 'page4.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Button Navigation App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page1()),
+                );
+              },
+              child: Text('Go to Page 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page2()),
+                );
+              },
+              child: Text('Go to Page 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page3()),
+                );
+              },
+              child: Text('Go to Page 3'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page4()),
+                );
+              },
+              child: Text('Go to Page 4'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
